c594f0a41a9c88ce448f15df788790d6aa022207
Charlie <a51095@hotmail.com>
2021-07-16 17:01:45 +0800

    【feature】新增App配置类;

---
+++ Config/CCAppKeys.swift
@@ -0,0 +1,12 @@
+//
+//  CCAppKeys.swift
+//  HelloSwift
+//
+//  Created by a51095 on 2021/7/16.
+//
+
+import Foundation
+
+struct CCAppKeys {
+    static let demoKey: String = "demoKey"
+}
---
+++ Config/CCAppNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  CCAppNotificationName.swift
+//  HelloSwift
+//
+//  Created by a51095 on 2021/7/16.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let CCDemoNotification = Notification.Name("CCDemoNotification")
+}
