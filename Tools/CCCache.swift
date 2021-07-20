3b5232dd8cbdacaef34f0fe553a92e0bb14f7375
Charlie <a51095@hotmail.com>
2021-07-20 09:34:15 +0800

    【feature】新增Cache缓存方法(命名规则,方法名规范);

--- Tools/CCCache.swift
+++ Tools/CCCache.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class CCCache {
     /// 字符串类型缓存"String"
-    static var store: Storage = try! Storage(diskConfig: DiskConfig(name: "CCCache"), memoryConfig: MemoryConfig(), transformer: TransformerFactory.forCodable(ofType: String.self))
+    static var store: Storage = try! Storage(diskConfig: DiskConfig(name: "disk_cache"), memoryConfig: MemoryConfig(), transformer: TransformerFactory.forCodable(ofType: String.self))
     /// 数组字符串类型缓存[String]
     private static var stringArrayStore = store.transformCodable(ofType: [String].self)
     /// 字典类型缓存 [String: Sting]
@@ -23,52 +23,50 @@ class CCCache {
         try? store.setObject(value, forKey: forkey)
     }
 
-    class func string(forKey: String) -> String? {
-        return try? store.object(forKey: forKey)
+    class func string(forKey key: String) -> String? {
+        return try? store.object(forKey: key)
     }
     
-    class func setDic(_ value: [String: Any], forkey: String) {
-        guard let jsonValue = value.jsonString() else {
+    class func setDic(_ dic: [String: Any], forkey: String) {
+        guard let jsonValue = dic.jsonString() else {
             removeObject(forKey: forkey)
             return
         }
         try? store.setObject(jsonValue, forKey: forkey)
     }
     
-    class func dic(forKey: String) -> [String: Any]? {
-        guard let jsonString = try? store.object(forKey: forKey), let dict = try? jsonString.toDict() as? [String: Any] else { return nil }
+    class func dic(forKey key: String) -> [String: Any]? {
+        guard let jsonString = try? store.object(forKey: key), let dict = try? jsonString.toDict() as? [String: Any] else { return nil }
         return dict
     }
     
-    class func setStringArray(_ value: [String]?, forkey: String) {
-        guard let value = value else {
+    class func setStringArray(_ array: [String]?, forkey: String) {
+        guard let value = array else {
             removeObject(forKey: forkey)
             return
         }
         try? stringArrayStore.setObject(value, forKey: forkey)
     }
 
-    class func stringArray(forKey: String) -> [String]? {
-        return try? stringArrayStore.object(forKey: forKey)
+    class func stringArray(forKey key: String) -> [String]? {
+        return try? stringArrayStore.object(forKey: key)
     }
 
-    class func setStringDic(_ value: [String: String]?, forkey: String) {
-        guard let value = value else {
+    class func setStringDic(_ dic: [String: String]?, forkey: String) {
+        guard let value = dic else {
             removeObject(forKey: forkey)
             return
         }
         try? stringDicStore.setObject(value, forKey: forkey)
     }
 
-    class func stringDic(forKey: String) -> [String: String]? {
-        return try? stringDicStore.object(forKey: forKey)
+    class func stringDic(forKey key: String) -> [String: String]? {
+        return try? stringDicStore.object(forKey: key)
     }
 
-    class func removeObject(forKey: String) {
-        try? store.removeObject(forKey: forKey)
+    class func removeObject(forKey key: String) {
+        try? store.removeObject(forKey: key)
     }
 
-    class func removeAll() {
-        try? store.removeAll()
-    }
+    class func removeAll() { try? store.removeAll() }
 }
