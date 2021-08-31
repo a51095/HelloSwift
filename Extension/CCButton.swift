d75d12f4d188b234e5ecf7c29e9a64b5a1515de2
Charlie <13204117850@163.com>
2021-08-31 14:55:51 +0800

    Update  CCButton.swift
    1）优化调整；

--- Extension/CCButton.swift
+++ Extension/CCButton.swift
@@ -5,7 +5,7 @@
 //  Created by a51095 on 2021/7/15.
 //
 
-enum Position { case top, right, bottom, left, center }
+enum Position { case top, right, bottom, left }
 
 extension UIButton {
     /// 重置图片image与标题title位置
@@ -15,22 +15,18 @@ extension UIButton {
         let imageWidth = self.imageView?.image?.size.width
         let imageHeight = self.imageView?.image?.size.height
         
-        let labelWidth = self.titleLabel?.intrinsicContentSize.width
-        let labelHeight = self.titleLabel?.intrinsicContentSize.height
+        let labelWidth = self.titleLabel?.frame.size.width
+        let labelHeight = self.titleLabel?.frame.size.height
         
         switch position {
-        case .center:
-            imageEdgeInsets = UIEdgeInsets(top: -labelHeight! - spacing / 2, left: imageWidth! / 2, bottom: 0, right: 0)
-            titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageWidth!, bottom: -imageHeight!-spacing/2, right: 0)
-            break
         case .top:
             imageEdgeInsets = UIEdgeInsets(top: -labelHeight! - spacing / 2, left: 0, bottom: 0, right: -labelWidth!)
             titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageWidth!, bottom: -imageHeight! - spacing / 2, right: 0)
             break
             
         case .left:
-            imageEdgeInsets = UIEdgeInsets(top: 0, left: -spacing / 2, bottom: 0, right: spacing)
-            titleEdgeInsets = UIEdgeInsets(top: 0, left: spacing / 2, bottom: 0, right: -spacing / 2)
+            imageEdgeInsets = UIEdgeInsets(top: 0, left: -spacing / 2, bottom: 0, right: 0)
+            titleEdgeInsets = UIEdgeInsets(top: 0, left: spacing * 1.5, bottom: 0, right: 0)
             break
             
         case .bottom:
