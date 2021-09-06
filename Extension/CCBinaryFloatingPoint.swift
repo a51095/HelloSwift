33aec24516149751c64001db510df0f42a28e0ba
Charlie <13204117850@163.com>
2021-09-06 14:07:08 +0800

    1)格式化代码;

--- Extension/CCBinaryFloatingPoint.swift
+++ Extension/CCBinaryFloatingPoint.swift
@@ -5,17 +5,13 @@
 //  Created by a51095 on 2021/7/15.
 //
 
-extension BinaryFloatingPoint{
+extension BinaryFloatingPoint {
     /// 浮点型转整型
-    var i: Int {
-        return Int(self)
-    }
+    var i: Int { return Int(self) }
     
     /// 小数点后如果只是0,取整显示;反之,则显示原值;
      var cleanZero : String {
-        if let i = Int(exactly: self) {
-            return String(i)
-        }
+        if let i = Int(exactly: self) { return String(i) }
         return String(Double(self))
     }
 }
