7eb058cbc3c9bf53b849eb3cbc959d57a6938d08
Charlie <13204117850@163.com>
2021-09-14 20:30:08 +0800

    【add】1)新增首次安装,引导视图; 2)新扩展一个随机颜色; 3)格式化代码;

--- Config/CCAppKeys.swift
+++ Config/CCAppKeys.swift
@@ -14,6 +14,8 @@
 @_exported import Kingfisher
 
 struct CCAppKeys {
+    /// 用户第一次安装标识符
+    static let firstKey: String = "user_first_install"
     static let freeUid: String = "11773"
     static let freeAppKey: String = "933f80b6d72c9a27d09169f311771cbe"
 }
