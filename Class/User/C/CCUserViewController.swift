eeca4c03c861f35972c0abdf2a8f0f8d9b9eb0f6
Charlie <13204117850@163.com>
2021-09-26 19:53:30 +0800

    【add】: 新增左右滑动缩放视图;

--- Class/User/C/CCUserViewController.swift
+++ Class/User/C/CCUserViewController.swift
@@ -4,7 +4,10 @@
 //
 //  Created by a51095 on 2021/7/15.
 //
+import UIKit
 
 class CCUserViewController: CCViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
