00bb768d68e9523a9906fa5a565b661e0b188781
Charlie <a51095@hotmail.com>
2021-10-19 19:29:35 +0800

    【update】: showErrorMsg

--- Network/CCNetwork.swift
+++ Network/CCNetwork.swift
@@ -21,7 +21,7 @@ struct CCResponseData {
 /// - completionHandle:       响应回调结果(逃逸闭包)
 
 /// 通用的网络请求方法
-func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: Dictionary<String, Any> = [:], showError: Bool = true, encoding: URLEncoding = .default, completionHandle: @escaping (CCResponseData) -> Void) {
+func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String: Any] = [:], showErrorMsg: Bool = true, encoding: URLEncoding = .default, completionHandle: @escaping (CCResponseData) -> Void) {
     // 公共参数
     var commonParam = ["uid": CCAppKeys.freeUid, "appkey": CCAppKeys.freeAppKey] as [String: Any]
     // 合并请求参数
@@ -32,7 +32,7 @@ func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: Diction
         kAppDelegate().window?.hideLoading()
         // 容错处理,若请求报错error,则直接返回默认数据
         guard res.error == nil else {
-            if showError {
+            if showErrorMsg {
                 kAppDelegate().window?.toast("出错啦", type: .failure)
             }
             completionHandle(CCResponseData());
@@ -41,7 +41,7 @@ func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: Diction
         
         // 容错处理,若请求res.value空值,则直接返回默认数据
         guard let dic = res.value as? [String: Any] else {
-            if showError {
+            if showErrorMsg {
                 kAppDelegate().window?.toast("出错啦", type: .failure)
             }
             completionHandle(CCResponseData());
@@ -50,7 +50,7 @@ func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: Diction
         
         // 容错处理,若请求code空值,则直接返回默认数据
         guard let code = dic["code"] as? String else {
-            if showError {
+            if showErrorMsg {
                 kAppDelegate().window?.toast("出错啦", type: .failure)
             }
             completionHandle(CCResponseData());
