2b03ffade14795533debd1c25ed75ec839057b07
Charlie <13204117850@163.com>
2021-10-19 19:35:41 +0800

    【update】: response

--- Network/CCNetwork.swift
+++ Network/CCNetwork.swift
@@ -9,7 +9,7 @@
 /// 自定义响应数据结构(目的在于封装的请求方法仅一个回调即可获取成功与失败两种状态)
 struct CCResponseData {
     var resCode: Int?
-    var data: Dictionary<String, Any>?
+    var data: [String: Any]?
 }
 
 // MARK: - NetworkRequest
@@ -18,10 +18,10 @@ struct CCResponseData {
 /// - parameters:      请求参数(Dictionary<String, Any>类型)
 /// - showError:                    响应错误信息(可自定义默认值,String类型)
 /// - encoding:                     请求编码格式(URLEncoding默认类型)
-/// - completionHandle:       响应回调结果(逃逸闭包)
+/// - response:                     响应回调结果(逃逸闭包)
 
 /// 通用的网络请求方法
-func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String: Any] = [:], showErrorMsg: Bool = true, encoding: URLEncoding = .default, completionHandle: @escaping (CCResponseData) -> Void) {
+func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String: Any] = [:], showErrorMsg: Bool = true, encoding: URLEncoding = .default, response: @escaping ((CCResponseData) -> Void)) {
     // 公共参数
     var commonParam = ["uid": CCAppKeys.freeUid, "appkey": CCAppKeys.freeAppKey] as [String: Any]
     // 合并请求参数
@@ -35,7 +35,7 @@ func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String
             if showErrorMsg {
                 kAppDelegate().window?.toast("出错啦", type: .failure)
             }
-            completionHandle(CCResponseData());
+            response(CCResponseData());
             return
         }
         
@@ -44,7 +44,7 @@ func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String
             if showErrorMsg {
                 kAppDelegate().window?.toast("出错啦", type: .failure)
             }
-            completionHandle(CCResponseData());
+            response(CCResponseData());
             return
         }
         
@@ -53,12 +53,12 @@ func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String
             if showErrorMsg {
                 kAppDelegate().window?.toast("出错啦", type: .failure)
             }
-            completionHandle(CCResponseData());
+            response(CCResponseData());
             return
         }
         
         let data = CCResponseData(resCode: code.i, data: dic)
-        completionHandle(data)
+        response(data)
     }
 }
 
