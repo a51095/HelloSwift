bdfa120df609b969c224b3fed13507277a0d8fc8
Charlie <a51095@hotmail.com>
2021-12-08 16:07:44 +0800

    【add】: 无 【update】: 格式化

--- Extension/ExObject.swift
+++ Extension/ExObject.swift
@@ -7,8 +7,8 @@
 
 public extension NSObject {
     /// 获取当前类的名称字符串(静态方法)
-    static func classString() -> String { NSStringFromClass(self.classForCoder()) }
+    static func classString() -> String { NSStringFromClass(classForCoder()) }
     
     /// 获取当前对象所属类的名称字符串(对象方法)
-    func classString() -> String { NSStringFromClass(self.classForCoder) }
+    func classString() -> String { NSStringFromClass(classForCoder) }
 }
