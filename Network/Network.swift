7c64f0555fa57331e83cd8b65dbb5bb862e5fe72
Charlie <a51095@hotmail.com>
2023-02-23 11:01:31 +0800

    0223
    格式化代码

--- Network/Network.swift
+++ Network/Network.swift
@@ -16,15 +16,14 @@ struct ResponseData {
     }
 }
 
-// MARK: - NetworkRequest
-/// - url:                                 请求接口API(String类型)
-/// - method:                        请求方法(默认post)
-/// - parameters:      请求参数(Dictionary<String, Any>类型)
-/// - showError:                    响应错误信息(可自定义，默认值,String类型)
-/// - encoding:                     请求编码格式(URLEncoding默认类型)
-/// - response:                     响应回调结果(逃逸闭包)
-
-// MARK: 通用的网络请求方法
+/// 通用的网络请求方法
+/// - Parameters:
+///   - url: 请求接口API(String类型)
+///   - method: 请求方法(默认post)
+///   - parameters: 请求参数(Dictionary<String, Any>类型)
+///   - showErrorMsg: 响应错误信息(可自定义，默认值,String类型)
+///   - encoding: 请求编码格式(URLEncoding默认类型)
+///   - response: 响应报文(逃逸闭包)
 func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String: Any] = [:], showErrorMsg: Bool = true, encoding: ParameterEncoding = URLEncoding.httpBody, response: @escaping ((ResponseData) -> Void)) {
     // 公共参数
     var commonParam = ["uid": AppKey.freeUid, "appkey": AppKey.freeAppKey] as [String: Any]
