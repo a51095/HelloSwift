51c4e2ac2328216fdbdb40981377ba91e9cf3cf1
Charlie <a51095@hotmail.com>
2023-03-20 13:33:11 +0800

    【**Notes**】 --------------------------------------- - addExBundle

---
+++ Extension/ExBundle.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loadData = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+
+        return loadData
+    }
+}
