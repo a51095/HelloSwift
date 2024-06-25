57ec56dfff4305d5e746be91ace1af16e93c59d5
Charlie <a51095@hotmail.com>
2024-06-25 14:12:13 +0800

    Update ExView.swift
    【update】currentViewController

--- Extension/ExView.swift
+++ Extension/ExView.swift
@@ -1,16 +1,18 @@
 extension UIView {
     /// 获取当前view所属vc
     func currentViewController() -> UIViewController? {
-        for view in sequence(first: self.superview, next: { $0?.superview }) {
-            if let responder = view?.next {
-                if responder.isKind(of: UIViewController.self) {
-                    return responder as? UIViewController
-                }
+        var nextResponder: UIResponder? = self
+    
+        while let responder = nextResponder {
+            if let viewController = responder as? UIViewController {
+                return viewController
             }
+            nextResponder = responder.next
         }
+    
         return nil
     }
-    
+
     /// 将View转换为UIImage(屏幕截图)
     func toImage() -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(self.frame.size, false, UIScreen.main.scale)
