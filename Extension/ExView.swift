76fda4a9fae15b8b156e6fbe53769d5fbbc967cf
Charlie <a51095@hotmail.com>
2024-06-25 14:15:55 +0800

    Update ExView.swift
    【update】toImage

--- Extension/ExView.swift
+++ Extension/ExView.swift
@@ -15,11 +15,20 @@ extension UIView {
 
     /// 将View转换为UIImage(屏幕截图)
     func toImage() -> UIImage? {
+        // 检查视图大小是否有效
+        guard self.frame.size.width > 0 && self.frame.size.height > 0 else { return nil }
+    
+        // 创建图像上下文
         UIGraphicsBeginImageContextWithOptions(self.frame.size, false, UIScreen.main.scale)
-        let context = UIGraphicsGetCurrentContext()
-        self.layer.render(in: context!)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img
+        defer { UIGraphicsEndImageContext() }
+    
+        // 获取当前上下文
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+    
+        // 渲染图层到上下文
+        self.layer.render(in: context)
+    
+        // 获取当前图像    
+        return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
