4e13ff79d63434a4eb21fd57cdbf632ca2ece2de
Charlie <a51095@hotmail.com>
2024-07-08 13:50:50 +0800

    Update AppKey.swift
    update unsplashKey

--- Config/AppKey.swift
+++ Config/AppKey.swift
@@ -6,4 +6,6 @@ struct AppKey {
     static let hasInstallKey: String = "has_install"
     /// the news key
     static let newsKey: String = "ce7c786dd0f990eabd175663be4b51bd"
+    /// the unsplash key
+    static let unsplashKey: String = "GmDzgvI3nWnKKxyDWX04qF51tlIv9cX6eIBzmsdJhT4"
 }
