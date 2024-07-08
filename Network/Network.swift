1468d18956994dc2fdfa6a7fcf6f418b6c754b8a
Charlie <a51095@hotmail.com>
2024-07-08 13:32:55 +0800

    Update Network.swift
    update NetworkRequest

--- Network/Network.swift
+++ Network/Network.swift
@@ -1,12 +1,55 @@
-/// 通用的网络请求方法
+enum ResponseType {
+    case array
+    case dictionary
+}
+
 /// - Parameters:
 ///   - url: 请求接口API(String类型)
-///   - method: 请求方法(默认post)
+///   - method: 请求方法(HTTPMethod)
 ///   - parameters: 请求参数(Dictionary<String, Any>类型)
 ///   - showErrorMsg: 响应错误信息(可自定义，默认值,String类型)
-///   - encoding: 请求编码格式(URLEncoding默认类型)
+///   - encoding: 请求编码格式(URLEncoding)
+///   - responseType: 响应报文类型(ResponseType))
 ///   - response: 响应报文(逃逸闭包)
-func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String: Any] = [:], showErrorMsg: Bool = true, encoding: ParameterEncoding = URLEncoding.httpBody, response: @escaping (([String: Any]?) -> Void)) {
+
+/// method - get
+// #mark - NetworkRequest
+func NetworkRequest(url: String, method: HTTPMethod = .get, showErrorMsg: Bool = true, encoding: ParameterEncoding = URLEncoding.default, responseType: ResponseType = .array, response: @escaping ((Any?) -> Void)) {
+    
+    DispatchQueue.main.async { kAppDelegate.window!!.showLoading() }
+    AF.request(url, method: method, parameters: nil, encoding: encoding).response { (res: AFDataResponse) in
+        DispatchQueue.main.async { kAppDelegate.window!!.hideLoading() }
+        
+        switch res.result {
+        case .success(let data):
+            switch responseType {
+            case .array:
+                let object = try? JSONSerialization.jsonObject(with: data!, options: [])
+                if let array = object as? [[String: Any]] {
+                    response(array)
+                } else {
+                    if showErrorMsg { kAppDelegate.window!!.toast("出错啦", type: .failure); return }
+                    response(nil)
+                }
+            case .dictionary:
+                let object = try? JSONSerialization.jsonObject(with: data!, options: [])
+                if let dictionary = object as? [String: Any] {
+                    response(dictionary)
+                } else {
+                    if showErrorMsg { kAppDelegate.window!!.toast("出错啦", type: .failure); return }
+                    response(nil)
+                }
+            }
+        case .failure(let error):
+            if showErrorMsg { kAppDelegate.window!!.toast(error.localizedDescription, type: .failure); return }
+            response(nil)
+        }
+    }
+}
+
+/// method - post
+// #mark - NetworkRequest
+func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String: Any] = [:], showErrorMsg: Bool = true, encoding: ParameterEncoding = URLEncoding.httpBody, responseType: ResponseType = .dictionary, response: @escaping ((Any?) -> Void)) {
     
     DispatchQueue.main.async { kAppDelegate.window!!.showLoading() }
     AF.request(url, method: method, parameters: parameters, encoding: encoding).response { (res: AFDataResponse) in
@@ -14,12 +57,23 @@ func NetworkRequest(url: String, method: HTTPMethod = .post, parameters: [String
         
         switch res.result {
         case .success(let data):
-            let object = try? JSONSerialization.jsonObject(with: data!, options: .fragmentsAllowed)
-            if let dic = object as? [String: Any] {
-                response(dic)
-            } else {
-                if showErrorMsg { kAppDelegate.window!!.toast("出错啦", type: .failure); return }
-                response(nil)
+            switch responseType {
+            case .array:
+                let object = try? JSONSerialization.jsonObject(with: data!, options: [])
+                if let array = object as? [[String: Any]] {
+                    response(array)
+                } else {
+                    if showErrorMsg { kAppDelegate.window!!.toast("出错啦", type: .failure); return }
+                    response(nil)
+                }
+            case .dictionary:
+                let object = try? JSONSerialization.jsonObject(with: data!, options: [])
+                if let dictionary = object as? [String: Any] {
+                    response(dictionary)
+                } else {
+                    if showErrorMsg { kAppDelegate.window!!.toast("出错啦", type: .failure); return }
+                    response(nil)
+                }
             }
         case .failure(let error):
             if showErrorMsg { kAppDelegate.window!!.toast(error.localizedDescription, type: .failure); return }
